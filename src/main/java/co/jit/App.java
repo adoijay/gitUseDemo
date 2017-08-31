54d2ea51ecc33554b45bfd482222d3bd9e8255aa
adoijay-win <adoijay-win@163.com>
2017-08-31 16:52:38 +0800

    分支后 win提交2

--- src/main/java/co/jit/App.java
+++ src/main/java/co/jit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Y4 004! fengzhi 02" );
+        System.out.println( "Y4 004! fengzhi 03" );
     }
 }
